60f9a0aa32c1ea860c224fcc0f324b99216794e1
VanillaPWNZ(Malk0lm) <48512277+V3nilla@users.noreply.github.com>
2022-09-07 22:59:21 +0400

    Initial Release

---
+++ IW4X-Fps-Boost/IW4XTool.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace IW4X_Fps_Boost
+{
+    partial class IW4XTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IW4XTool));
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProcOpenLabel = new System.Windows.Forms.Label();
+            this.FPSBoost = new System.Windows.Forms.CheckBox();
+            this.Bob = new System.Windows.Forms.CheckBox();
+            this.Lunge = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.WorkerReportsProgress = true;
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker_DoWork);
+            this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
+            this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IW4X:";
+            // 
+            // ProcOpenLabel
+            // 
+            this.ProcOpenLabel.AutoSize = true;
+            this.ProcOpenLabel.BackColor = System.Drawing.Color.White;
+            this.ProcOpenLabel.ForeColor = System.Drawing.Color.Red;
+            this.ProcOpenLabel.Location = new System.Drawing.Point(47, 9);
+            this.ProcOpenLabel.Name = "ProcOpenLabel";
+            this.ProcOpenLabel.Size = new System.Drawing.Size(27, 13);
+            this.ProcOpenLabel.TabIndex = 1;
+            this.ProcOpenLabel.Text = "N/A";
+            // 
+            // FPSBoost
+            // 
+            this.FPSBoost.AutoSize = true;
+            this.FPSBoost.BackColor = System.Drawing.Color.White;
+            this.FPSBoost.ForeColor = System.Drawing.Color.Black;
+            this.FPSBoost.Location = new System.Drawing.Point(15, 30);
+            this.FPSBoost.Name = "FPSBoost";
+            this.FPSBoost.Size = new System.Drawing.Size(76, 17);
+            this.FPSBoost.TabIndex = 2;
+            this.FPSBoost.Text = "FPS Boost";
+            this.FPSBoost.UseVisualStyleBackColor = false;
+            this.FPSBoost.CheckedChanged += new System.EventHandler(this.FPSBoost_CheckedChanged);
+            // 
+            // Bob
+            // 
+            this.Bob.AutoSize = true;
+            this.Bob.BackColor = System.Drawing.Color.White;
+            this.Bob.ForeColor = System.Drawing.Color.Black;
+            this.Bob.Location = new System.Drawing.Point(15, 52);
+            this.Bob.Name = "Bob";
+            this.Bob.Size = new System.Drawing.Size(103, 17);
+            this.Bob.TabIndex = 3;
+            this.Bob.Text = "Disable Bobbing";
+            this.Bob.UseVisualStyleBackColor = false;
+            this.Bob.CheckedChanged += new System.EventHandler(this.Bob_CheckedChanged);
+            // 
+            // Lunge
+            // 
+            this.Lunge.AutoSize = true;
+            this.Lunge.BackColor = System.Drawing.Color.White;
+            this.Lunge.ForeColor = System.Drawing.Color.Black;
+            this.Lunge.Location = new System.Drawing.Point(15, 73);
+            this.Lunge.Name = "Lunge";
+            this.Lunge.Size = new System.Drawing.Size(270, 17);
+            this.Lunge.TabIndex = 4;
+            this.Lunge.Text = "Infinite Knife Lunge (Require Commando PRO Perk)";
+            this.Lunge.UseVisualStyleBackColor = false;
+            this.Lunge.CheckedChanged += new System.EventHandler(this.Lunge_CheckedChanged);
+            // 
+            // IW4XTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(300, 96);
+            this.Controls.Add(this.Lunge);
+            this.Controls.Add(this.Bob);
+            this.Controls.Add(this.FPSBoost);
+            this.Controls.Add(this.ProcOpenLabel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "IW4XTool";
+            this.ShowIcon = false;
+            this.Text = "IW4X Tool v0.7.5-dev x86";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProcOpenLabel;
+        private System.Windows.Forms.CheckBox FPSBoost;
+        private System.Windows.Forms.CheckBox Bob;
+        private System.Windows.Forms.CheckBox Lunge;
+    }
+}
+
---
+++ IW4X-Fps-Boost/IW4XTool.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+
+namespace IW4X_Fps_Boost
+{
+    public partial class IW4XTool : Form
+    {
+        public Mem m = new Mem();
+        public IW4XTool()
+        {
+            InitializeComponent();
+        }
+
+        bool ProcOpen = false;
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ProcOpen = m.OpenProcess("iw4x");
+            Thread.Sleep(1000);
+            BGWorker.ReportProgress(0);
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+            {
+                ProcOpenLabel.ForeColor = Color.Green;
+                ProcOpenLabel.Text = "Found";
+            }
+            else
+            {
+                ProcOpenLabel.ForeColor = Color.Red;
+                ProcOpenLabel.Text = "N/A";
+            }
+        }
+
+
+        private void BGWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void FPSBoost_CheckedChanged(object sender, EventArgs e)
+        {
+            if (FPSBoost.Checked)
+            {
+                m.WriteMemory("base+60064C0", "int", "0");
+                m.WriteMemory("base+6008CC0", "int", "0");
+            }
+            else
+            {
+                m.WriteMemory("base+60064C0", "int", "1");
+                m.WriteMemory("base+6008CC0", "int", "1");
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            m.WriteMemory("base+60064C0", "int", "1");
+            m.WriteMemory("base+6008CC0", "int", "1");
+            m.WriteMemory("base+60119B0", "float", "8");
+            m.WriteMemory("base+6011C30", "float", "8");
+            m.WriteMemory("base+60126D0", "float", "0.5");
+            m.WriteMemory("base+6014520", "float", "176");
+            m.WriteMemory("base+6014530", "float", "176");
+            Environment.Exit(0);
+        }
+
+        private void Bob_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Bob.Checked)
+            {
+                m.WriteMemory("base+60119B0", "float", "0");
+                m.WriteMemory("base+6011C30", "float", "0");
+                m.WriteMemory("base+60126D0", "float", "0");
+            }
+            else
+            {
+                m.WriteMemory("base+60119B0", "float", "8");
+                m.WriteMemory("base+6011C30", "float", "8");
+                m.WriteMemory("base+60126D0", "float", "0.5");
+            }
+        }
+
+        private void Lunge_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Lunge.Checked)
+            {
+                m.WriteMemory("base+6014520", "float", "9999");
+                m.WriteMemory("base+6014530", "float", "9999");
+            }
+            else
+            {
+                m.WriteMemory("base+6014520", "float", "176");
+                m.WriteMemory("base+6014530", "float", "176");
+            }
+        }
+    }
+}
