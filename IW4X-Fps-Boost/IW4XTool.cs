f0e106ebc72a221a92fce229a502053ff070fa55
VanillaPWNZ(Malk0lm) <48512277+V3nilla@users.noreply.github.com>
2022-09-09 17:09:54 +0400

    useless thing

--- IW4X-Fps-Boost/IW4XTool.cs
+++ IW4X-Fps-Boost/IW4XTool.cs
@@ -60,7 +60,6 @@ namespace IW4X_Fps_Boost
             {
                 m.UnfreezeValue("base+60064C0");
                 m.UnfreezeValue("base+6008CC0");
-                Thread.Sleep(200);
                 m.WriteMemory("base+60064C0", "int", "1");
                 m.WriteMemory("base+6008CC0", "int", "1");
             }
@@ -107,7 +106,6 @@ namespace IW4X_Fps_Boost
                 m.UnfreezeValue("base+60119B0");
                 m.UnfreezeValue("base+6011C30");
                 m.UnfreezeValue("base+60126D0");
-                Thread.Sleep(200);
                 m.WriteMemory("base+60119B0", "float", "8");
                 m.WriteMemory("base+6011C30", "float", "8");
                 m.WriteMemory("base+60126D0", "float", "0.5");
@@ -125,7 +123,6 @@ namespace IW4X_Fps_Boost
             {
                 m.UnfreezeValue("base+6014520");
                 m.UnfreezeValue("base+6014530");
-                Thread.Sleep(200);
                 m.WriteMemory("base+6014520", "float", "176");
                 m.WriteMemory("base+6014530", "float", "176");
             }
